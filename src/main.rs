f6cb362489faf4c9ec2a8464cb4f63585fff095a
leofigy <angel.figueroa@digitalonus.com>
2020-10-19 16:41:09 -0500

    spend ..

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,11 @@ fn main() {
     for i in 2..args.len() as usize {
         match args.get(i){
             Some(filename) => {
-                let contents = fs::read_to_string(filename)
-                .expect("Something went wrong reading the file");
-                println!("With text:\n{}", contents);
+                let out = match fs::read_to_string(filename){
+                    Ok(content) => content,
+                    Err(fatal) => fatal.to_string()
+                };
+                println!("{}",out);
             },
             _ => println!("nothing to get")
         }
