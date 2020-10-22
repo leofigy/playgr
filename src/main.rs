a2d57c8eb6a8b1b4eb08c4ca26c82556fdec1b22
leofigy <angel.figueroa@digitalonus.com>
2020-10-22 08:58:56 -0500

    sample complete

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct Config {
+    filename : String,
+    content : String,
+}
+
+#[derive(Debug)]
+struct Search {
+    query: String,
+    data : Vec<Config>,
+}
+
+
 fn main() {
     let args : Vec<String> = env::args().collect();
     
@@ -9,18 +22,41 @@ fn main() {
         return
     }
 
-    let pattern = &args[1];
+    let mut search_data = Search{
+        query: args[1].clone(),
+        data: Vec::new(),
+    };
 
     for i in 2..args.len() as usize {
         match args.get(i){
             Some(filename) => {
-                let out = match fs::read_to_string(filename){
-                    Ok(content) => content,
-                    Err(fatal) => fatal.to_string()
+                match fs::read_to_string(filename){
+                    Ok(content) => {
+                        search_data.data.push(Config{
+                            filename: filename.to_string(),
+                            content: content,
+                        })
+                    },
+                    Err(fatal) => println!("{}", fatal)
                 };
-                println!("{}",out);
             },
             _ => println!("nothing to get")
         }
     }
+
+    for data in search_data.data{
+        println!("{:?}", data);
+        let result = search(&search_data.query, &data.content);
+        println!("matches found {:?}", result);
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+  let mut results = Vec::new();
+  for line in contents.lines(){
+      if line.contains(query){
+        results.push(line);
+      }
+  }
+  results
 }
